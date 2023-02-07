3fe5f0f69ce2aa9b4181cd490586d744417ca8bb
Taisa Braga <taisabraga.adm@gmail.com>
2023-02-07 10:45:31 -0300

    implements the configuration to populate database

---
+++ src/main/java/config/TestConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import entities.User;
+import repositories.UserRepository;
+
+@Component
+public class TestConfig implements CommandLineRunner {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+	  User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", "123456");
+	  User u2 = new User("Alex Green", "alex@gmail.com", "977777777", "123456");
+	  userRepository.saveAll(Arrays.asList(u1,u2));
+  }
+}
