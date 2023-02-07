87ee4edd8bb758a7da89f32b5b8eca64cac2f6da
Taisa Braga <taisabraga.adm@gmail.com>
2023-02-07 14:50:57 -0300

    fix UserService update

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -10,25 +10,29 @@ import entities.User;
 import repositories.UserRepository;
 
 @Service
-public interface UserService {
+public class UserService {
 
-	@Autowired
-	public static final UserRepository userRepository = null;
+	@Autowired 
+	private UserRepository userRepository;
 	
-	public static List<User> findAll(){
+	public List<User> findAll(){
 	   return userRepository.findAll();
 	}
 	
-	public static Optional<User> findById(Long id){
+	public Optional<User> findById(Long id){
 		   return userRepository.findById(id);
 	}
 	
-    public static void save(User user) {
+    public void save(User user) {
     	userRepository.save(user);
     }
     
-    public static void delete(Long id) {
+    public void delete(Long id) {
     	userRepository.deleteById(id);
+    }
+    
+    public void update(User user) {
+    	userRepository.save(user);
     }
     
 }
